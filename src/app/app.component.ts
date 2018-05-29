bf2209684cc096c729a0207710185bcb8e410a77
muyuanqiang <yuanqiang.mou@funi365.com>
2018-05-29 17:35:58 +0800

    initialize commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loading = true;
+
+  antButtonClick(event) {
+    this.loading = !this.loading;
+    console.log(event);
+  }
 }
+
